f106dc1b7d56f55b399cbea9435b694404ce0f87
Robert Martin <rmartin@rmart.in>
2017-04-14 17:19:10 -0400

    [carto] Improve Carto uploads
    - Parse URL to get upload destination
    - Use carto-python 1.0.0

--- geopetl/carto.py
+++ geopetl/carto.py
@@ -1,24 +1,44 @@
+import os
+import tempfile
 import petl as etl
 from petl import Table
+from geopetl.util import parse_carto_url
 
 
 def tocarto(table, url, geom_field=None):
-    from cartodb import CartoDBAPIKey, CartoDBException, FileImport
+    from carto.auth import APIKeyAuthClient
+    from carto.file_import import FileImportJob
 
-    filename = name + '.csv'
+    # parse url
+    try:
+        parsed = parse_carto_url(url)
+        domain = parsed['host']
+        table_name = parsed['table_name']
+        try:
+            api_key = parsed['query']['API_KEY']
+        except KeyError:
+            raise ValueError('Carto API key not supplied')
+    except ValueError:
+        raise ValueError('Invalid Carto URL')
+
+    temp_dir = tempfile.gettempdir()
+    temp_file_path = os.path.join(temp_dir, table_name + '.csv')
 
     # write to csv
     # TODO can FileImport take a stream so we don't have to write to csv first?
     (etl.reproject(table, 4326, geom_field=geom_field)
         .rename('shape', 'the_geom')
-        .tocsv(filename)
+        .tocsv(temp_file_path)
     )
 
-    API_KEY = '<api key>'
-    DOMAIN = '<domain>'
-    cl = CartoDBAPIKey(API_KEY, DOMAIN)
-    fi = FileImport(filename, cl, privacy='public')
-    fi.run()
+    # import to carto
+    base_url = 'https://{}.carto.com'.format(domain)
+    auth_client = APIKeyAuthClient(base_url, api_key)
+    file_import_job = FileImportJob(temp_file_path, auth_client)
+    file_import_job.run(table_name=table_name)
+
+    # clean up temp file
+    os.remove(temp_file_path)
 
 def _tocarto(self, *args, **kwargs):
     tocarto(self, *args, **kwargs)
