8d887cf7c659604a01a6975881b3c4e28a88f2f7
Robert Martin <rmartin@rmart.in>
2018-02-15 13:34:40 -0500

    Use setup.py to list/install dependencies

--- setup.py
+++ setup.py
@@ -2,5 +2,11 @@ from setuptools import setup, find_packages
 
 setup(name='geopetl',
       version='0.0.0',
-      packages=find_packages()
+      packages=find_packages(),
+      install_requires=['petl'],
+      extras_require={
+          'oracle_sde': ['cx_Oracle'],
+          'postgis': ['psycopg2'],
+          'carto': ['carto'],
+      }
      )
