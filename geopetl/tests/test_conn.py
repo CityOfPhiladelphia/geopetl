3b76a5e81dac32888609c4a2560c13ec7c40259d
Victor Espinoza <Victor.Espinoza@phila.gov>
2020-04-30 14:20:34 -0400

    pytest script for connection to db using psycopg.

---
+++ geopetl/tests/test_conn.py
@@ -0,0 +1,15 @@
+import psycopg2
+
+def test_connectToPostgres(db, user, pw):
+    c = False
+    try:
+        connection = psycopg2.connect(user=user, password=pw , database=db)
+        cursor = connection.cursor()
+        print("connected to postgres DB ")
+        c = True
+        assert c == True
+
+    except(Exception, psycopg2.Error):
+        print("postgres error ", psycopg2.Error)
+        c = False
+        assert c == False
