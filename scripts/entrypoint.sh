754963eeb0b5bc2525226e40b11023e7955b3ae9
floptical <stephen.macdavid@gmail.com>
2020-05-22 18:21:28 -0400

    Fix env var check function in entrypoint
    Tested it as working but I was mistaken.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+
 #export LC_ALL=en_US.utf-8
 #export LANG=en_US.utf-8
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
 # check to make sure all environment variables that we'll need were declared.
-# I don't know why but only a second 'echo' shows up in docker-compose output.
-[ -z $POSTGRES_HOST ] && echo ""; echo "ERROR! POSTGRES_HOST env var is undeclared! Exiting unsuccessfully.."; exit 1
-[ -z $POSTGRES_USER ] && echo ""; echo "ERROR! POSTGRES_USER env var is undeclared! Exiting unsuccessfully.."; exit 1
-[ -z $POSTGRES_PASSWORD ] && echo ""; echo "ERROR! POSTGRES_PASSWORD env var is undeclared! Exiting unsuccessfully.."; exit 1
-[ -z $POSTGRES_DB ] && echo ""; echo "ERROR! POSTGRES_DB env var is undeclared! Exiting unsuccessfully.."; exit 1
+# Check if variable does not exist or is zero-length
+[[ ! -v POSTGRES_HOST || -z "$POSTGRES_HOST" ]] &&  echo "ERROR! POSTGRES_HOST env var is undeclared or empty! Exiting unsuccessfully.." && exit 1
+[[ ! -v POSTGRES_USER || -z "$POSTGRES_USER" ]] &&  echo "ERROR! POSTGRES_USER env var is undeclared or empty! Exiting unsuccessfully.." && exit 1
+[[ ! -v POSTGRES_PASSWORD || -z "$POSTGRES_PASSWORD" ]] &&  echo "ERROR! POSTGRES_PASSWORD env var is undeclared or empty! Exiting unsuccessfully.." && exit 1
+[[ ! -v POSTGRES_DB || -z "$POSTGRES_DB" ]] &&  echo "ERROR! POSTGRES_DB env var is undeclared or empty! Exiting unsuccessfully.." && exit 1
 
 # Not installing postgres client package to keep docker container small
 # So here's a python script to use psycopg2 to see if the databse is up and ready for connections
