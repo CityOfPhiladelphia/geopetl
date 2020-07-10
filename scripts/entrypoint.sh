e755e7c2cc1742603c6097d6aac521d4142982b5
floptical <stephen.macdavid@gmail.com>
2020-07-10 12:04:03 -0400

    Make things work

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,21 +5,20 @@
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
-POSTGRES_SDE_HOST='postgres-sde'
+#psql will read the password from this variable automatically:
+export PGPASSWORD=$POSTGRES_PASSWORD
+# Since we're doing health checks via a subshell-made variable, we'll need to export
+# our variables so they're accessible.
+export POSTGIS_DB
+export POSTGIS_HOST
+export POSTGIS_USER
+export SDE_DB
+export SDE_HOST
+export SDE_USER
+export POSTGRES_PASSWORD
 
-# check to make sure all environment variables that we'll need were declared.
-# Check if variable does not exist or is zero-length
-#[[ ! -v POSTGRES_HOST || -z "$POSTGRES_HOST" ]] &&  echo "ERROR! POSTGRES_HOST env var is undeclared or empty! Exiting unsuccessfully.." && exit 1
-#[[ ! -v POSTGRES_USER || -z "$POSTGRES_USER" ]] &&  echo "ERROR! POSTGRES_USER env var is undeclared or empty! Exiting unsuccessfully.." && exit 1
-#[[ ! -v POSTGRES_PASSWORD || -z "$POSTGRES_PASSWORD" ]] &&  echo "ERROR! POSTGRES_PASSWORD env var is undeclared or empty! Exiting unsuccessfully.." && exit 1
-#[[ ! -v POSTGRES_DB || -z "$POSTGRES_DB" ]] &&  echo "ERROR! POSTGRES_DB env var is undeclared or empty! Exiting unsuccessfully.." && exit 1
-
-# Not installing postgres client package to keep docker container small
-# So here's a python script to use psycopg2 to see if the databse is up and ready for connections
-pg_postgis_ready=$(python_pg_isready.py --host $POSTGIS_HOST --user $POSTGIS_USER --password $POSTGRES_PASSWORD --dbname $POSTGIS_DB --port 5432; echo $?)
-pg_sde_ready=$(python_pg_isready.py --host $SDE_HOST --user $SDE_USER --password $POSTGRES_PASSWORD --dbname $SDE_DB --port 5432; echo $?)
-echo "pg_postgis_ready return is:" $pg_postgis_ready
-echo "pg_sde_ready return is" $pg_sde_ready
+pg_postgis_ready=$(pg_isready -h $POSTGIS_HOST -U $POSTGIS_USER -d $POSTGIS_DB &>/dev/null; echo $? )
+pg_sde_ready=$(pg_isready -h $SDE_HOST -U $SDE_USER  -d $SDE_DB &>/dev/null; echo $? )
 
 max_retry=20
 counter=0
@@ -28,10 +27,12 @@ do
   [[ counter -eq $max_retry ]] && echo "Failed!" && exit 1
   echo "PostGIS or postgres-sde database is not ready yet!!"
   sleep 15
-  pg_postgis_ready=$(python_pg_isready.py --host $POSTGIS_HOST --user $POSTGIS_USER --password $POSTGRES_PASSWORD --dbname $POSTGIS_DB --port 5432; echo $?)
-  pg_sde_ready=$(python_pg_isready.py --host $SDE_HOST --user $SDE_USER --password $POSTGRES_PASSWORD --dbname $SDE_DB --port 5432; echo $?)
-  echo "pg_postgis_ready return is:" $pg_postgis_ready
-  echo "pg_sde_ready return is" $pg_sde_ready
+  pg_isready -h $POSTGIS_HOST -U $POSTGIS_USER -d $POSTGIS_DB
+  pg_isready -h $SDE_HOST -U $SDE_USER  -d $SDE_DB
+  pg_postgis_ready=$(pg_isready -h $POSTGIS_HOST -U $POSTGIS_USER -d $POSTGIS_DB &>/dev/null; echo $? )
+  pg_sde_ready=$(pg_isready -h $SDE_HOST -U $SDE_USER  -d $SDE_DB &>/dev/null; echo $? )
+#  echo "pg_postgis_ready return is:" $pg_postgis_ready
+#  echo "pg_sde_ready return is" $pg_sde_ready
   ((counter++))
 done
 echo "Both databases are ready and accepting conections."
@@ -43,6 +44,7 @@ pytest geopetl/tests/test_postgis.py \
   --pw=$POSTGRES_PASSWORD \
   --db=$POSTGIS_DB \
   --host=$POSTGIS_HOST \
+  --port=5432 \
   --schema="geopetl/tests/fixtures_data/schemas/point.json"
 
 # test for postgres-sde
