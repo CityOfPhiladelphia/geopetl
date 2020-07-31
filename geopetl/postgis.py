fa9d353966033987c1728868e2fb7ce3932b006f
Alexander-M-Waldman <alex.waldman@phila.gov>
2020-07-31 17:25:20 +0000

    Add datatypes to field_type_map

--- geopetl/postgis.py
+++ geopetl/postgis.py
@@ -297,6 +297,9 @@ class PostgisDatabase(object):
 FIELD_TYPE_MAP = {
     'smallint':                 'num',
     'integer':                  'num',
+    'smallint':                 'num',
+    'float':                    'num',
+    'double':                   'num',
     'numeric':                  'num',
     'double precision':         'num',
     'text':                     'text',
