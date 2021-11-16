4cf6fc454afca220384c8cf736a02be6055e95be
Victor Espinoza <vespinoza0391@gmail.com>
2021-11-16 11:17:25 -0500

    Edit create boolean.

--- geopetl/postgis.py
+++ geopetl/postgis.py
@@ -81,7 +81,7 @@ def topostgis(rows, dbo, table_name, from_srid=None, column_definition_json=None
     # do we need to create the table?
     table = db.table(table_name)
     # sample = 0 if create else None # sample whole table
-    create = table.name_with_schema not in db.tables
+    create = '.'.join([table.schema, table.name]) not in db.tables
     # Create table if it doesn't exist
     if create:
         # Disable autocreate new postgres table
