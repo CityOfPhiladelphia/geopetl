2a52fdaec6852055722a63e0c468f87a39b19f0c
Victor Espinoza <vespinoza0391@gmail.com>
2022-01-12 15:23:45 -0500

    Edit prepare_geom method.

--- geopetl/postgis.py
+++ geopetl/postgis.py
@@ -521,9 +521,11 @@ class PostgisTable(object):
     def _prepare_geom(self, geom, srid, transform_srid=None, multi_geom=True):
         """Prepares WKT geometry by projecting and casting as necessary."""
         # if DB is postgis enabled
-        if self.db.postgis_version != '' and not self.db.sde_version:
-            geom = "ST_GeomFromText('{}', {})".format(geom, srid) if geom and geom != 'EMPTY' else "null"
-        else: # if DB is not Postgis enabled
+        try:
+            if self.db.postgis_version != '' and not self.db.sde_version:
+                geom = "ST_GeomFromText('{}', {})".format(geom, srid) if geom and geom != 'EMPTY' else "null"
+        except:
+            #else: # if DB is not Postgis enabled
             geom = "ST_GEOMETRY('{}', {})".format(geom, srid) if geom and geom != 'EMPTY' else "null"
 
         # Handle 3D geometries
