a4ca6a64cec11a5db40abc889fd6eb912bf54230
Victor Espinoza <vespinoza0391@gmail.com>
2022-03-15 16:00:22 -0400

    Edit removewhitespace method.

--- geopetl/tests/tests_config.py
+++ geopetl/tests/tests_config.py
@@ -1,12 +1,12 @@
 import re
 import re
-point_table_name = 'point_table'
+point_table_name = ''
 point_csv_dir = 'geopetl/tests/fixtures_data/staging/point.csv'
 
-polygon_table_name = 'polygon_table'
+polygon_table_name = ''
 polygon_csv_dir = 'geopetl/tests/fixtures_data/staging/polygon.csv'
 
-line_table_name = 'line_table'
+line_table_name = ''
 line_csv_dir = 'geopetl/tests/fixtures_data/staging/line.csv'
 
 
@@ -14,27 +14,22 @@ point_column_definition = 'geopetl/tests/fixtures_data/schemas/point.json'
 polygon_column_definition = 'geopetl/tests/fixtures_data/schemas/polygon.json'
 line_column_definition = 'geopetl/tests/fixtures_data/schemas/line.json'
 
+srid_precision = {'2272':1,
+                  '4326':1,}
 
-def remove_whitespace(stringval,srid=None):
-    shapestring = str(stringval)
-    geom_type = re.findall("[A-Z]{1,12}", shapestring)[0]
-    coordinates = re.findall(r"[-+]?\d*\.\d+|\d+", shapestring)
-    if srid:
-        srid = int(srid)
-    if srid == 4326:
-        #truncate coordinates to 8 decimal places
-        coordinates= [str(float('%.8f' % float(coords))) for coords in coordinates]
-    elif srid == 2272:
-        #truncate coordinates to 3 decimal places
-        coordinates= [str(float('%.3f' % float(coords))) for coords in coordinates]
-    else:
-        #truncate coordinates to 4 decimal places
-        coordinates= [str(float('%.6f' % float(coords))) for coords in coordinates]
+fields= {
+    'text_field_name':'',
+    'numeric_field_name':'',
+    'date_field_name':'',
+    'timestamp_field_name':'',
+    'timezone_field_name':'',
+    'object_id_field_name':'',
+    'shape_field_name':''
+          }
 
-    if geom_type.lower()  == 'point':
-        geom = "{type}({x})".format(type=geom_type, x=" ".join(coordinates))
-    elif geom_type.lower()  == 'polygon':
-        geom = "{type}(({x}))".format(type=geom_type, x=" ".join(coordinates))
-    elif geom_type.lower() == 'linestring':
-        geom = "{type}(({x}))".format(type=geom_type, x=" ".join(coordinates))
-    return geom
+def remove_whitespace(geom_wkt,srid):
+    geom_wkt = str(geom_wkt)
+    geom_type = re.findall("[A-Z]{1,12}", geom_wkt)[0]
+    coordinates = re.findall(r"[-+]?\d*\.\d+|\d+", geom_wkt)
+    coordinates_list = [float("%.{}f".format(srid_precision.get(srid)) % float(coords)) for coords in coordinates]
+    return geom_type, coordinates_list
