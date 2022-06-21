79ad8601fee08a940466f94543e51679f7853f01
Roland MacDavid <roland.macdavid@phila.gov>
2022-06-21 18:25:27 +0000

    First pass oracle docker stuff and workflow

---
+++ scripts/oracle_entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#export LC_ALL=en_US.utf-8
+#export LANG=en_US.utf-8
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
+export ORACLE_USER
+export ORACLE_PW
+export ORACLE_DB
+export ORACLE_HOST
+
+# Note: the hostname postgis is a docker-made DNS record
+# When you specify the container name in docker-compose.yml
+echo ""
+echo "#########################################"
+echo "Running tests against Oracle database..."
+pytest geopetl/tests/test_oracle.py \
+  --user=$ORACLE_USER \
+  --pw=$ORACLE_PASSWORD \
+  --service_name=$ORACLE_DB \
+  --host=$ORACLE_HOST \
+  --port=1521
+
+ORACLE_EXIT_CODE=$?
+exit $?
