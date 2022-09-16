9dd0b090c88f36309fe84db04a4ee9e005f523cd
floptical <stephen.macdavid@gmail.com>
2022-09-16 20:45:43 +0000

    Cleanup

--- scripts/sde_rds_tests_entrypoint.sh
+++ scripts/sde_rds_tests_entrypoint.sh
@@ -9,15 +9,7 @@ export LANG=C.UTF-8
 #export PGPASSWORD=$POSTGRES_PASSWORD
 # Since we're doing health checks via a subshell-made variable, we'll need to export
 
-echo 'postgissde entrypoint debug ....'
-echo $POSTGISSDE_DB
-echo $POSTGISSDE_USER
-echo $POSTGISSDE_HOST
-echo $POSTGISSDE_PASSWORD
-echo '------------------------------'
-
-
-pg_postgis_ready=$(pg_isready -h $POSTGISSDE_HOST -U $POSTGISSDE_USER -d $POSTGISSDE_DB &>/dev/null; echo $? )
+pg_postgis_ready=$(pg_isready -h $HOST -U $USER -d $DB &>/dev/null; echo $? )
 
 max_retry=20
 counter=0
@@ -26,13 +18,13 @@ do
   [[ counter -eq $max_retry ]] && echo "Failed!" && exit 1
   echo "PostGIS database is not ready yet!!"
   sleep 15
-  pg_isready -h $POSTGISSDE_HOST -U $POSTGISSDE_USER -d $POSTGISSDE_DB
-  pg_postgis_ready=$(pg_isready -h $POSTGISSDE_HOST -U $POSTGISSDE_USER -d $POSTGISSDE_DB &>/dev/null; echo $? )
+  pg_isready -h $HOST -U $USER -d $DB
+  pg_postgis_ready=$(pg_isready -h $HOST -U $USER -d $DB &>/dev/null; echo $? )
 #  echo "pg_postgis_ready return is:" $pg_postgis_ready
 #  echo "pg_sde_ready return is" $pg_sde_ready
   ((counter++))
 done
-echo "Postgis database is ready and accepting conections."
+echo "database is ready and accepting conections."
 
 #python -m pytest
 
@@ -40,29 +32,29 @@ echo "Postgis database is ready and accepting conections."
 # When you specify the container name in docker-compose.yml
 echo ""
 echo "#########################################"
-echo "Running 2272 tests against PostGIS database..."
+echo "Running 2272 tests against SDE RDS database..."
 pytest -vvv -ra --disable-warnings --showlocals --tb=native geopetl/tests/test_postgis.py \
-  --user=$POSTGISSDE_USER \
-  --pw=$POSTGISSDE_PASSWORD \
-  --db=$POSTGISSDE_DB \
-  --host=$POSTGISSDE_HOST \
+  --user=$USER \
+  --pw=$PASSWORD \
+  --db=$DB \
+  --host=$HOST \
   --port=5432 \
-  --schema=$POSTGISSDE_USER \
+  --schema=$USER \
   --srid=2272
-POSTGIS_EXIT_CODE=$?
+EXIT_CODE=$?
 
-if [[ "$POSTGIS_EXIT_CODE" -ne "0"  ]]; then
-    echo "Errors encountered in 2272 postgis tests."
+if [[ "EXIT_CODE" -ne "0"  ]]; then
+    echo "Errors encountered in 2272 tests."
     exit 1
 fi
 
 echo "#########################################"
-echo "Running 4326 tests against PostGIS database..."
+echo "Running 4326 tests against SDE RDS database..."
 pytest -vvv -ra --disable-warnings --showlocals --tb=native geopetl/tests/test_postgis.py \
-  --user=$POSTGISSDE_USER \
-  --pw=$POSTGISSDE_PASSWORD \
-  --db=$POSTGISSDE_DB \
-  --host=$POSTGISSDE_HOST \
+  --user=$USER \
+  --pw=$PASSWORD \
+  --db=$DB \
+  --host=$HOST \
   --port=5432 \
   --schema="public" \
   --srid=4326
