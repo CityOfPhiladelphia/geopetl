b6a13fae287b6fb4c307132742caa966a4d57283
Victor Espinoza <Victor.Espinoza@phila.gov>
2023-02-06 15:10:16 -0500

    add date util req

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(name='geopetl',
       version='0.0.1',
       packages=find_packages(),
-      install_requires=['petl','psycopg2','cx_Oracle'],
+      install_requires=['petl','psycopg2','cx_Oracle','python-dateutil'],
       extras_require={
           'oracle_sde': ['cx_Oracle'],
           'postgis': ['psycopg2']
